ba39618973e0674344c024a97c45233e7e7c1eda
Dag Robøle <dag.robole@gmail.com>
2016-03-04 09:53:37 +0100

    Changed path to Osprey SDK

--- spec_proc.py
+++ spec_proc.py
@@ -25,7 +25,7 @@ from array import array
 import os, sys, math, copy, time, socket, threading, logging
 
 # Import API for the detector
-toolkitPath = os.getcwd() + os.path.sep + "../osprey"
+toolkitPath = os.getcwd() + os.path.sep + "../DataTypes"
 sys.path.append(toolkitPath)
 
 from DeviceFactory import *
